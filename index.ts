348b45ba0303bca0fbeecb9c3cc686ce38b51728
Axure <axurez@vip.qq.com>
2015-06-16 11:39:19 +0800

    A failed attempt to utilize TypeScript.

--- index.ts
+++ index.ts
@@ -1,8 +1,53 @@
 /// <reference path="./typings/rx/rx.d.ts" />
+/// <reference path="./typings/rx/rx.async.d.ts" />
+/// <reference path="./typings/rx/rx.time.d.ts" />
 /// <reference path="./typings/jquery/jquery.d.ts" />
+/// <reference path="./typings/rx-jquery/rx.jquery.d.ts" />
 
 import Rx = require('rx');
 import $ = require('jquery');
+
+var throttledInput = $('#textInput')
+    .keyupAsObservable()
+    .map( function (ev) {
+        return $(ev.target).val();
+    })
+    .filter( function (text) {
+        return text.length > 2;
+    })
+    .throttle(500)
+    .distinctUntilChanged();
+
+function searchWikipedia(term: string) {
+    return $.ajaxAsObservable({
+        url: 'http://en.wikipedia.org/w/api.php',
+        data: { action: 'opensearch',
+            search: term,
+            format: 'json' },
+        dataType: 'jsonp'
+    });
+}
+
+
+var suggestions = throttledInput.flatMapLatest( function (text) {
+    return searchWikipedia(text);
+});
+
+
+var selector = $('#results');
+
+var subscription = suggestions.subscribe(
+    function (data) {
+        selector.empty();
+        $.each(data[1], function (_, text: string) {
+            $('<li>' + text + '</li>').appendTo(selector);
+        });
+    },
+    function (e) {
+        selector.empty();
+        $('<li>Error: ' + e + '</li>').appendTo('#results');
+    }
+);
 
 //import Rx from 'rx';
 //import $ from 'jquery';
@@ -15,32 +60,32 @@ import $ = require('jquery');
 //var Subscriber = Rx.Subscription;
 
 
-var observer = Rx.Observer.create(
-    (ev) => {
-        alert(ev);
-    },
-    (err) => {
-        alert(`Error with ${err}`);
-    },
-    () => {
-        alert('Done!');
-    }
-);
-
-var $input = $('#input'),
-    $results = $('#results');
-
-var text: string = "This is a fcuk";
-
-//console.log($input.value);
-console.log($input);
-$input.val("This is fuck");
-console.log($input.val());
-
-$input.val(text);
-
-/* Only get the value from each key up */
-//var keyups = Rx.Observable.fromEvent($input, 'keyup')
+//var observer = Rx.Observer.create(
+//    (ev) => {
+//        alert(ev);
+//    },
+//    (err) => {
+//        alert(`Error with ${err}`);
+//    },
+//    () => {
+//        alert('Done!');
+//    }
+//);
+//
+//var input = $('#input'),
+//    results = $('#results');
+//
+//var text: string = "This is a fcuk";
+//
+////console.log(input.value);
+//console.log(input);
+//input.val("This is fuck");
+//console.log(input.val());
+//
+//input.val(text);
+//
+///* Only get the value from each key up */
+//var keyups = Rx.Observable.fromEvent(input, 'keyup')
 //    .map(function (e) {
 //        return e.target.value;
 //    })
